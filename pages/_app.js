43ca1249aec9e3c35cf2f10ba3a8d62bcf66812f
Jake <jacob@liveineverynow.com>
2023-07-27 19:15:48 -0700

    Color switcher implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from 'next-themes';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,87 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+// import { useTheme } from 'next-themes';
+// import { useEffect } from 'react';
+import ThemeChanger from '../components/ThemeChanger';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+
+// const themes = [
+//   { name: 'Light' },
+//   { name: 'Dark' },
+//   { name: 'Monakai' },
+//   { name: 'Pink' },
+// ];
 
 export default function Home() {
+  // const [mounted, setMounted] = useState(false);
+  // const { resolvedTheme, setTheme } = useTheme();
+
+  // // When mounted on client, now we can show the UI
+  // useEffect(() => setMounted(true), []);
+
+  // if (!mounted) return null;
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="antialiased font-sans h-full w-full bg-th-background text-th-primary-dark">
+      <ThemeChanger />
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      >
+        <div className="z-10 w-full max-w-xl items-center justify-between font-mono font-thin text-sm lg:flex">
+          <div className="flex-col space-y-16">
+            {/* <button
+            onClick={() => {
+              setTheme(resolvedTheme === 'dark' ? 'light' : 'dark');
+            }}
+            selectedTheme={resolvedTheme}
+            className="p-4 bg-black dark:bg-white rounded-full"
+          ></button> */}
+            <p className="uppercase">Create with Care</p>
+            <div className="space-y-8">
+              <p className="indent-8">
+                From nothing a new thing exists, imbued with the essence of the
+                maker.
+              </p>
+              <p>Good in = good out. Bad in = bad out.</p>
+              <p>
+                For instance, a maker optimizing for money is likely to end up
+                with a product that: uses poor material, does not pay people
+                along the supply chain adequately, is made by a machine, and
+                promotes very little response from the end user.
+              </p>
+              <p>
+                A screenwriter optimizing for the truth within themselves could
+                make the movie-goer cry tears of joy.
+              </p>
+              <p>
+                It is much harder to find the truth within oneself than it is to
+                set up a drop shipping store.
+              </p>
+              <p>--</p>
+              <p>We are all both makers and consumers.</p>
+              <p>
+                Our environment consists of a surplus of things to consume made
+                both with and without care. It is hard for the end-user to make
+                good decisions about what to consume.
+              </p>
+              <p>
+                It is much easier to tell when something you’ve made doesn’t
+                meet the mark. Thus, the impetus is on makers to create with
+                care every single time.
+              </p>
+              <p>
+                If all makers create with care every time there is only care to
+                consume.
+              </p>
+              <p>--</p>
+              <p>
+                This is a moral pursuit. To make in this way is a noble act. In
+                order to fill a world with care we create with care.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </div>
+  );
 }
